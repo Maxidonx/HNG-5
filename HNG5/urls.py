f658d00a872b9a0eb7f7cc0cfe33b91cf7078898
Maxdonx <johnmaxwell927@gmail.com>
2023-10-02 11:15:46 +0100

    done

---
+++ HNG5/urls.py
@@ -0,0 +1,58 @@
+"""
+URL configuration for HNG5 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from HNG5API.views import CreateRecordingView, GetDataView, MergeRecordingView, AllRecordingsView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Screen recording API",
+        default_version='v1',
+        description="Screen recording API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/create/', CreateRecordingView.as_view()),
+    path('api/all/', AllRecordingsView.as_view()),
+    path('api/save-data/<int:id>/', GetDataView.as_view()),
+    path('api/merge-data/', MergeRecordingView.as_view()),
+    # path('api/recording/<int:id>/', VideoRecordingsView.as_view()),
+    # path('api/video/<int:id>/', SingleVideoView.as_view()),
+    # path('api/stream/<int:id>/', RecordingVideoView.as_view()),
+
+
+
+
+
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ HNG5API/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from compat import StringIO
+
+# Create your models here.
+
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', '-updated_at']
+
+class Recordings(TimeStamp):
+    title = models.CharField(max_length=300, blank=True, null=True)
+    name = models.CharField(max_length=270, blank=True, null=True)
+    transcript = models.TextField(blank=True, null=True)
+    video = models.FileField(("Video File"), upload_to='videos/', null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    
+    
+    
+    
+    def __str__(self):
+        return self.name 
+    
+    
+# class VideoRecordings(TimeStamp):
+#     recording = models.ForeignKey(Recordings, on_delete=models.CASCADE)
+#     video = models.FileField(("Video File"), upload_to='videos/', null=True)
+#     transcript = models.TextField(blank=True, null=True)
+    
+    
+#     def __str__(self):
+#         return self.recordings.name + ": " + str(self.video)
