0f82e10aa1315225ab05d70a9e713bdfaa4484b2
Maxdonx <johnmaxwell927@gmail.com>
2023-10-03 17:35:44 +0100

    starting again

--- HNG5API/tasks.py
+++ HNG5API/tasks.py
@@ -1,42 +1,58 @@
-import boto3
-from botocore.exceptions import WaiterError
-from django.conf import settings
-import time
-from rest_framework.response import Response
+# import whisper
 from celery import shared_task
-
-# Initialize the Transcribe client
-transcribe = boto3.client('transcribe', region_name=settings.AWS_S3_REGION_NAME)
-
-
-@shared_task(bind=True)
-def start_transcription(self, input_uri):
-    try:
-        job_name = f'TranscriptionJob_{int(time.time())}'
-        
-        # Ensure that input_uri is a string before splitting
-        if isinstance(input_uri, str):
-            # Extract the input video file name from the input_uri
-            input_video_name = input_uri.split('/')[-1]
-            print(f"INput URI: {input_uri}")
-            print(f"INput video name: {input_video_name}")
-            # Use the input video name as the base name for the output key
-            output_key = f'transcripts/{input_video_name}.json'
+from django.conf import settings
+import os
+from django.core.files.storage import default_storage
+from pydub import AudioSegment
+from .models import Video
+from moviepy.editor import VideoFileClip
+from .serializers import TranscriptionSerializer
+from asgiref.sync import sync_to_async
 
 
-        # Start transcription job
-        response = transcribe.start_transcription_job(
-            TranscriptionJobName=job_name,
-            Media={'MediaFileUri': input_uri},
-            MediaFormat='mp4',
-            LanguageCode='en-US',  # Change the language code as needed
-            OutputBucketName=settings.AWS_STORAGE_BUCKET_NAME,
-            OutputKey=output_key,  # Adjust the output key as needed
-        )
 
-        # Return a response indicating that the job has started
-        return {'message': 'Transcription job started', 'job_name': job_name}
+@shared_task
+def merge_recording(recording_id):
+    if recording_id:
+        try:
+            recording = Video.objects.get(id=recording_id)
+            folder_name = recording.name
 
-    except WaiterError as e:
-        # Handle any WaiterError exceptions if needed
-        return {'message': f'Error: {str(e)}', 'status': 500}
+            folder_path = os.path.join(
+                settings.BASE_DIR, 'media', folder_name)
+
+            video_files = [f for f in os.listdir(
+                folder_path) if f.endswith('.mp4')]
+
+            if len(video_files) < 2:
+                return 'Not enough video files to merge'
+
+            video_files.sort()
+
+            file_paths = [os.path.join(folder_path, video_file)
+                          for video_file in video_files]
+
+            video_clips = [VideoFileClip(file_path)
+                           for file_path in file_paths]
+
+            final_clip = video_clips[0]
+            for clip in video_clips[1:]:
+                final_clip = final_clip.set_duration(
+                    final_clip.duration + clip.duration)
+                final_clip = final_clip.set_end(
+                    final_clip.end + clip.duration)
+
+            final_video_path = os.path.join(folder_path, 'final_video.mp4')
+            final_clip.write_videofile(
+                final_video_path, codec='libx264', temp_audiofile='temp-audio.m4a', remove_temp=True)
+
+            for file_path in file_paths:
+                os.remove(file_path)
+
+            # transcribe_video(recording_id)
+
+            return 'Video files merged successfully'
+        except Video.DoesNotExist:
+            return 'Recording not found'
+    else:
+        return 'Recording ID not provided'
